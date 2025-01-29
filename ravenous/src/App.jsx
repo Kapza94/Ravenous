0ec1d7e486d04b8133efd667fa534a2842d7c515
Kapza <lukakapetanovic@icloud.com>
2025-01-29 15:14:50 +0100

    business cards done

--- ravenous/src/App.jsx
+++ ravenous/src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Business from "./Business";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Business />
+    </>
+  );
 }
 
 export default App;
---
+++ ravenous/src/Business.jsx
@@ -0,0 +1,157 @@
+import restaurantsImage from "./assets/steak.jpg";
+import "./business.css";
+
+const Business = () => {
+  return (
+    <>
+      <div className="grid grid-cols-3 gap-4">
+        <div className="main-card">
+          <div className="top-card">
+            <img
+              src={restaurantsImage}
+              alt="cover image with search bar beneath"
+              className="cover-img"
+            />
+          </div>
+          <div className="bottom-card">
+            <h3>MarginOTTO</h3>
+            <div className="flex justify-between">
+              <p>110 Paddington Way</p>
+              <p>Italian</p>
+            </div>
+            <div className="flex justify-between">
+              <p>BorderTown</p>
+              <p>4.5 stars</p>
+            </div>
+            <div className="flex justify-between">
+              <p>NY10101</p>
+              <p>90 reviews</p>
+            </div>
+          </div>
+        </div>
+        <div className="main-card">
+          <div className="top-card">
+            <img
+              src={restaurantsImage}
+              alt="cover image with search bar beneath"
+              className="cover-img"
+            />
+          </div>
+          <div className="bottom-card">
+            <h3>MarginOTTO</h3>
+            <div className="flex justify-between">
+              <p>110 Paddington Way</p>
+              <p>Italian</p>
+            </div>
+            <div className="flex justify-between">
+              <p>BorderTown</p>
+              <p>4.5 stars</p>
+            </div>
+            <div className="flex justify-between">
+              <p>NY10101</p>
+              <p>90 reviews</p>
+            </div>
+          </div>
+        </div>
+        <div className="main-card">
+          <div className="top-card">
+            <img
+              src={restaurantsImage}
+              alt="cover image with search bar beneath"
+              className="cover-img"
+            />
+          </div>
+          <div className="bottom-card">
+            <h3>MarginOTTO</h3>
+            <div className="flex justify-between">
+              <p>110 Paddington Way</p>
+              <p>Italian</p>
+            </div>
+            <div className="flex justify-between">
+              <p>BorderTown</p>
+              <p>4.5 stars</p>
+            </div>
+            <div className="flex justify-between">
+              <p>NY10101</p>
+              <p>90 reviews</p>
+            </div>
+          </div>
+        </div>
+        <div className="main-card">
+          <div className="top-card">
+            <img
+              src={restaurantsImage}
+              alt="cover image with search bar beneath"
+              className="cover-img"
+            />
+          </div>
+          <div className="bottom-card">
+            <h3>MarginOTTO</h3>
+            <div className="flex justify-between">
+              <p>110 Paddington Way</p>
+              <p>Italian</p>
+            </div>
+            <div className="flex justify-between">
+              <p>BorderTown</p>
+              <p>4.5 stars</p>
+            </div>
+            <div className="flex justify-between">
+              <p>NY10101</p>
+              <p>90 reviews</p>
+            </div>
+          </div>
+        </div>
+        <div className="main-card">
+          <div className="top-card">
+            <img
+              src={restaurantsImage}
+              alt="cover image with search bar beneath"
+              className="cover-img"
+            />
+          </div>
+          <div className="bottom-card">
+            <h3>MarginOTTO</h3>
+            <div className="flex justify-between">
+              <p>110 Paddington Way</p>
+              <p>Italian</p>
+            </div>
+            <div className="flex justify-between">
+              <p>BorderTown</p>
+              <p>4.5 stars</p>
+            </div>
+            <div className="flex justify-between">
+              <p>NY10101</p>
+              <p>90 reviews</p>
+            </div>
+          </div>
+        </div>
+        <div className="main-card">
+          <div className="top-card">
+            <img
+              src={restaurantsImage}
+              alt="cover image with search bar beneath"
+              className="cover-img"
+            />
+          </div>
+          <div className="bottom-card">
+            <h3>MarginOTTO</h3>
+            <div className="flex justify-between">
+              <p>110 Paddington Way</p>
+              <p>Italian</p>
+            </div>
+            <div className="flex justify-between">
+              <p>BorderTown</p>
+              <p>4.5 stars</p>
+            </div>
+            <div className="flex justify-between">
+              <p>NY10101</p>
+              <p>90 reviews</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Business;
