7943341a539a53cbe47834d1b744d97169af1ba8
Kapza <lukakapetanovic@icloud.com>
2025-01-30 22:06:37 +0100

    styling done adding API in next lesson

--- ravenous/src/Header.jsx
+++ ravenous/src/Header.jsx
@@ -11,6 +11,7 @@ const Header = () => {
   return (
     <>
       <div className="header-container img-bckround flex flex-col justify-between items-center pb-36 pt-36">
+        <div className="blurred-background"></div>
         <div className="heading-block">
           <BlurText
             text="Ravenous"
@@ -20,6 +21,22 @@ const Header = () => {
             onAnimationComplete={handleAnimationComplete}
             className="text-8xl text-white font-extrabold tracking-wide"
           />
+        </div>
+        <div className="filter-container">
+          <div className=" bordered flex justify-around">
+            <div className=" flex justify-center items-center flex-col">
+              <h3 className="filter-text text-white">Best</h3>
+              <h3 className="filter-text text-white">Match</h3>
+            </div>
+            <div className=" flex justify-center items-center flex-col">
+              <h3 className="filter-text text-white">Highest</h3>
+              <h3 className="filter-text text-white">Rated</h3>
+            </div>
+            <div className=" flex justify-center items-center flex-col">
+              <h3 className="filter-text text-white">Most</h3>
+              <h3 className="filter-text text-white">Reviewed</h3>
+            </div>
+          </div>
         </div>
         <div className="search-block">
           <input
@@ -38,7 +55,7 @@ const Header = () => {
         <div className="btn-block">
           <button className="bg-white pl-10 pr-10 pt-1 pb-1 rounded-lg">
             {" "}
-            <ShinyText text="Search" disabled={false} speed={3} className="bg-white " />
+            <ShinyText text="Search" disabled={false} speed={5} className="bg-white " />
           </button>
         </div>
       </div>
