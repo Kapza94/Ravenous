0b029b77b0d9c8f4bc83eca49e00edf7cb72b493
Kapza <lukakapetanovic@icloud.com>
2025-01-30 21:39:34 +0100

    added reactbits lib testing out some components.

--- ravenous/src/Header.jsx
+++ ravenous/src/Header.jsx
@@ -1,11 +1,13 @@
 // import React from "react";
 import "./header.css";
 import BlurText from "./BlurText";
+import ShinyText from "./ShinyText";
 
 const Header = () => {
   const handleAnimationComplete = () => {
     console.log("Animation completed!");
   };
+
   return (
     <>
       <div className="header-container img-bckround flex flex-col justify-between items-center pb-36 pt-36">
@@ -16,15 +18,28 @@ const Header = () => {
             animateBy="letters"
             direction="top"
             onAnimationComplete={handleAnimationComplete}
-            className="text-8xl text-white font-extrabold"
+            className="text-8xl text-white font-extrabold tracking-wide"
           />
         </div>
         <div className="search-block">
-          <input type="text" className="input-field mr-5 rounded-sm drop-shadow-md" id="biz-id" />
-          <input type="text" className="input-field rounded-sm drop-shadow-md" id="where-id" />
+          <input
+            type="text"
+            placeholder="Search Businesses?"
+            className="input-field mr-5 rounded-2xl drop-shadow-md pl-4 pr-40 pt-2 pb-2 bg-slate-200 outline-none"
+            id="biz-id"
+          />
+          <input
+            type="text"
+            placeholder="Where?"
+            className="input-field rounded-2xl drop-shadow-md pl-4 pr-40 pt-2 pb-2 bg-slate-200 outline-none"
+            id="where-id"
+          />
         </div>
         <div className="btn-block">
-          <button className="btn bg-white ">Search</button>
+          <button className="bg-white pl-10 pr-10 pt-1 pb-1 rounded-lg">
+            {" "}
+            <ShinyText text="Search" disabled={false} speed={3} className="bg-white " />
+          </button>
         </div>
       </div>
     </>
